a9ff512ccbc5b7660b89591302eb53aaf504e6e9
g-traub <guillaume.trb@hotmail.com>
2020-01-12 13:42:52 +0100

    added map

--- src/pages/Map/Map.js
+++ src/pages/Map/Map.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function App() {
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+function Map() {
+  const TOKEN = 'pk.eyJ1IjoiZ3VpbGxhdW1ldHJiIiwiYSI6ImNrNWF3aDV2MzFiZjkzbnBhM3M4anIybzQifQ.A0PdDZWEzaB3UxEgocXjCw';
+  
+  const mapStyle = 'mapbox://styles/guillaumetrb/ck5ayj7fu17og1cs5y26xe3vs'
+
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height: '100vh',
+    latitude: 48.857704,
+    longitude: 2.339466,
+    zoom: 11.5,
+  });
+
   return (
-    <div className="MapPage">
-      <div>Map</div>
-    </div>
+    <ReactMapGL
+      {...viewport}
+      onViewportChange={setViewport}
+      mapboxApiAccessToken={TOKEN}
+      mapStyle={mapStyle}
+    />
   );
 }
 
-export default App;
+export default Map;
