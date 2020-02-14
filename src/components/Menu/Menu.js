204d9d0e87fb8fc46b0555827693e04032294f8b
g-traub <guillaume.trb@hotmail.com>
2020-02-14 14:31:38 +0100

    menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -8,21 +8,21 @@ function Menu() {
   const [isMenuOpen, setisMenuOpen] = useState(false);
 
   return (
-    <section id="Menu">
+    <section id="Menu" className={isMenuOpen ? 'isOpen' : ''}>
       <div className="menu">
-        <div className="buttons">
-          <div className="button">
-            <div className="button__text">All</div>
-            <img className="button__icon" src={require('assets/icons/All.svg')} alt="" />
-          </div>
-          <div className="button">
-            <div className="button__text">Now</div>
-            <img className="button__icon button__icon--time" src={require('assets/icons/Time.svg')} alt="" />
-          </div>
-          <div onClick={() => setisMenuOpen(!isMenuOpen)} className="filter__button">{isMenuOpen ? 'Ok' : 'More filters'}</div>
+        <div className={isMenuOpen ? 'buttons buttons--bordered' : 'buttons'}>
+        <div className="button">
+          <div className="button__text">All</div>
+          <img className="button__icon" src={require('assets/icons/All.svg')} alt="" />
         </div>
+        <div className="button">
+          <div className="button__text">Now</div>
+          <img className="button__icon button__icon--time" src={require('assets/icons/Time.svg')} alt="" />
+        </div>
+        <div onClick={() => setisMenuOpen(!isMenuOpen)} className="filter__button">{isMenuOpen ? 'Ok' : 'More filters'}</div>
       </div>
-    </section>
+      </div>
+    </section >
   );
 }
 
