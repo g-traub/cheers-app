8b98993b0dd2e28db5d8adbfbb84886adbf83dc5
g-traub <guillaume.trb@hotmail.com>
2020-02-23 10:46:37 +0100

    picker done

--- src/components/Picker/Picker.js
+++ src/components/Picker/Picker.js
@@ -1,33 +1,65 @@
 import React, { useState, useLayoutEffect, useRef } from 'react';
-
 // style
 import './Picker.scss';
 
-function Picker(props) {
+const _debounce = require('lodash/debounce');
 
-  const eltRef = useRef(null);
-  const [dimensions, setDimensions] = useState({});
-
-  useLayoutEffect(() => {
-    setDimensions(eltRef.current.getBoundingClientRect());
-  }, []);
-
-  const values = props.values.map((value, index) => {
-    if (index === 0) {
-      return <li ref={eltRef} onClick={() => props.handleChange(value)} className="picker__value" key={index}>{value}</li>
+export default function Picker(props) {
+  const isIntersecting = (target, root) => {
+    const {right, left} = root.getBoundingClientRect()
+    const targetRect = target.getBoundingClientRect()
+    if (targetRect.right - targetRect.right * 0.1 < right && targetRect.left + targetRect.left * 0.1 > left) {
+      props.handleChange(target.textContent)
     }
-
-    return <li onClick={() => props.handleChange(value)} className="picker__value" key={index}>{value}</li>
-  })
+  }
+  
+  const handleScroll = event => {
+    const eventTarget = event.target
+      for (let childNode of eventTarget.childNodes) {
+        _debounce(() => isIntersecting(childNode, selectorRef.current), 100)()
+      }
+  }
+  
+  // data
+  const selectorRef = useRef(null);
+  const values = props.values.map((value, index) => {
+    return (
+      <PickerValue value={value} rootRef={selectorRef.current} selected={value === props.value} key={index} />
+    )
+  });
+  
+  // state
+  const [dimensions, setDimensions] = useState({});
+  
+  //hooks
+  useLayoutEffect(() => {
+    setDimensions({ height: '35px', width: '50px' });
+  }, []);
 
   return (
     <div id="Picker">
       <div className="picker__label">{props.label}</div>
       <div className="picker__sliderContainer">
-        <ol className="picker__values">{values}</ol>
-        <div className="picker__selector" style={{ width: dimensions.width, height: dimensions.height }}></div>
+        <div ref={selectorRef} className="picker__selector" style={{ width: dimensions.width, height: dimensions.height }}>
+          {Boolean(selectorRef.current) && <ol className="picker__values" onScroll={handleScroll}>{values}</ol>}
+        </div>
       </div>
     </div>
   )
 }
-export default Picker;
+
+function PickerValue(props) {
+  const ref = useRef(null)
+
+  const scrollTo = (elt, behaviour) => {
+    elt.scrollIntoView({ behavior: behaviour, inline: "center" });
+  }
+
+  useLayoutEffect(() => {
+    if(props.selected) scrollTo(ref.current, 'instant')
+  }, []);
+
+  return (
+    <li ref={ref} onClick={() => scrollTo(ref.current, 'smooth')} className="picker__value">{props.value}</li>
+  )
+}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,25 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 import logo from 'assets/logo_text.svg';
 import './HomePage.scss';
 
+import Picker from 'components/Picker/Picker'
 
 function App() {
+  const [value, setValue] = useState('6€');
+  const handleChange = newValue => {
+    setValue(newValue)
+  }
+
   return (
-    <div className="Home">
-      <img src={logo} className="App-logo" alt="logo" />
-      <div>
-        <p>Cheers will help you find the best place to share the Olympic Games’ emotions !</p>
-        <p>Go out and celebrate !</p>
-      </div>
-      <Link
-        className="App-link"
-        to="/map"
-      >
-        Start
-        </Link>
-    </div>
+    <Picker label="Less then" values={['1€', '2€', '3€', '4€', '5€', '6€', '7€', '8€', '9€', '10€']} value={value} handleChange={handleChange} />
+    // <div className="Home">
+    //   <img src={logo} className="App-logo" alt="logo" />
+    //   <div>
+    //     <p>Cheers will help you find the best place to share the Olympic Games’ emotions !</p>
+    //     <p>Go out and celebrate !</p>
+    //   </div>
+    //   <Link
+    //     className="App-link"
+    //     to="/map"
+    //   >
+    //     Start
+    //     </Link>
+    // </div>
   );
 }
 
